30c132721da5de44b2b395b947964a58472b2ebb
Natalia Samoilova <ladymarengo@gmail.com>
2020-03-06 19:11:21 +0200

    Solution for aoc2015-15

--- aoc2015-15.py
+++ aoc2015-15.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+
 test_input = '''Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
 Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3'''
 real_input = '''Sprinkles: capacity 5, durability -1, flavor 0, texture 0, calories 5
@@ -34,53 +35,35 @@ def making_list_of_ingredients(raw_input):
     return list_of_ingredients
 
 
-def making_recipe(ings, expected_volume):
-    list_of_recipes = []
-    recipe_lenght = len(ings)
-    def adding(ings, expected_volume, actual_volume=0, recipe_dict={}):
-        nonlocal list_of_recipes
-        for ing in ings:
-            for i in range(1, expected_volume):
-                new_actual_volume = actual_volume + i
-                new_recipe_dict = recipe_dict.copy()
-                new_recipe_dict[i] = ing
-                new_ings = list(ings)
-                new_ings.remove(ing)
-                if new_actual_volume < expected_volume:
-                    adding(new_ings, expected_volume, new_actual_volume, new_recipe_dict)
-                elif new_actual_volume == expected_volume and len(new_recipe_dict) == recipe_lenght:
-                    list_of_recipes.append(new_recipe_dict.copy())
-                    new_recipe_dict.clear()
-                else:
-                    break
-    adding(ings, expected_volume)
-    return list_of_recipes
+def making_amounts_for_2_ing(volume):
+    list_of_amounts = [(x, y) for x in range(1,volume) for y in range(1,volume) if x + y == volume]
+    return list_of_amounts
 
 
-def counting_scores(recipes_list):
+def making_amounts_for_4_ing(volume):
+    list_of_amounts = [(x, y, z, w) for x in range(1,volume) for y in range(1,volume)
+                       for z in range(1,volume) for w in range(1,volume) if x + y + z + w == volume]
+    return list_of_amounts
+
+
+def counting_scores_from_amounts(amounts_list, ing_list):
     scores = []
-    for recipe in recipes_list:
+    for amounts in amounts_list:
         total_capacity = 0
         total_durability = 0
         total_flavor = 0
         total_texture = 0
         total_calories = 0
-        for amount in recipe:
-            ing = recipe[amount]
+        for e, amount in enumerate(amounts):
+            ing = ing_list[e]
             total_capacity += ing.capacity * amount
             total_durability += ing.durability * amount
             total_flavor += ing.flavor * amount
             total_texture += ing.texture * amount
             total_calories += ing.calories * amount
-        if total_capacity < 0:
-            total_capacity = 0
-        if total_durability < 0:
-            total_durability = 0
-        if total_flavor < 0:
-            total_flavor = 0
-        if total_texture < 0:
-            total_texture = 0
-        if total_calories == 500:
+        if len(list(filter(lambda x: x > 0, [total_capacity, total_durability, total_flavor, total_texture]))) < 4 or total_calories != 500:
+            continue
+        else:
             score = total_capacity * total_durability * total_flavor * total_texture
             scores.append(score)
     return scores
@@ -88,10 +71,18 @@ def counting_scores(recipes_list):
 
 def main(raw_input, volume):
     list_of_ingredients = making_list_of_ingredients(raw_input)
-    list_of_recipes = making_recipe(list_of_ingredients, volume)
-    scores = counting_scores(list_of_recipes)
+    list_of_amounts = making_amounts_for_4_ing(volume)
+    scores = counting_scores_from_amounts(list_of_amounts, list_of_ingredients)
     return scores
 
 
-scores = main(test_input, 100)
-print(max(scores))
+def test(raw_input, volume):
+    list_of_ingredients = making_list_of_ingredients(raw_input)
+    list_of_amounts = making_amounts_for_2_ing(volume)
+    scores = counting_scores_from_amounts(list_of_amounts, list_of_ingredients)
+    return scores
+
+
+real_scores = main(real_input, 100)
+test_scores = test(test_input, 100)
+print(max(real_scores))
