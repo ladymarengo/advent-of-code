3b4e6685f7676dec29327d99c29a642911a42c94
Natalia Samoilova <ladymarengo@gmail.com>
2020-12-14 14:24:02 +0200

    Add final version of adventofcode Day 14

--- 2020/aoc2020_14.py
+++ 2020/aoc2020_14.py
@@ -15,13 +15,11 @@ def main(text):
         for i in range(start, len(key_value)):
             if mask[i] == '0':
                 new_string += key_value[i]
-                # print(i, new_string)
                 if i == len(key_value)-1:
                     list_mem_keys.append(new_string)
                     return True
             elif mask[i] == '1':
                 new_string += '1'
-                # print(i, new_string)
                 if i == len(key_value)-1:
                     list_mem_keys.append(new_string)
                     return True
@@ -39,15 +37,12 @@ def main(text):
 
     for instruction in instructions:
         result = re.search(r'(.+) = (.+)', instruction)
-        # print(result.group(1), result.group(2))
         if result.group(1) == 'mask':
             mask = result.group(2)
-            # print(mask)
         else:
             mem_result = re.search(r'mem\[([0-9]+)\]', result.group(1))
             mem_key = int(mem_result.group(1))
             value = int(result.group(2))
-            # print(mem_key, value)
             bin_value = "{:036b}".format(value)
             new_bin = ''
 
@@ -59,32 +54,18 @@ def main(text):
             mem1[mem_key] = int(new_bin, 2)
 
             key_bin_value = "{:036b}".format(mem_key)
-            # print(key_bin_value)
             list_mem_keys = []
             check_float(0, '', key_bin_value)
             for key in list_mem_keys:
                 mem2[int(key, 2)] = value
-            # print(mem2)
 
     sum1 = 0
     for key in mem1:
         sum1 += mem1[key]
-
     sum2 = 0
     for key in mem2:
         sum2 += mem2[key]
-    print(sum2)
+    print(sum1, sum2)
 
 
-test_input = '''mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-mem[8] = 11
-mem[7] = 101
-mem[8] = 0'''
-
-test_input2 = '''mask = 000000000000000000000000000000X1001X
-mem[42] = 100
-mask = 00000000000000000000000000000000X0XX
-mem[26] = 1'''
-
-main(test_input2)
 main(puzzle_input)
