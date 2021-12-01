30a28aed697cd7b98fc4c64890cd5888a305abb8
Natalia Samoilova <ladymarengo@gmail.com>
2021-12-01 15:50:09 +0200

    Move from folder

--- 2021-rust/day01.rs
+++ 2021-rust/day01.rs
@@ -12,8 +12,8 @@ fn main() {
 
 fn count_increase(interval: usize, measurements: &Vec<String>) -> i32 {
     let mut answer: i32 = 0;
-    for i in 3..measurements.len() {
-        if i > 2 && measurements[i].parse::<i32>().unwrap()
+    for i in interval..measurements.len() {
+        if i >= interval && measurements[i].parse::<i32>().unwrap()
             > measurements[i-interval].parse::<i32>().unwrap() {
             answer = answer + 1;
         }
