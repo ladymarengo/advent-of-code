e460484218b9cbbc5279db6771dbb5f299ddead1
Natalia Samoilova <samoilova.natalia.n@gmail.com>
2022-10-02 11:22:53 +0300

    First test

--- 2019-rust/src/01.rs
+++ 2019-rust/src/01.rs
@@ -1,3 +1,21 @@
 fn main() {
     println!("Hello world!");
 }
+
+fn calculate_fuel(mass: i32) -> i32 {
+    2
+}
+
+#[cfg(test)]
+mod test {
+    use test_case::test_case;
+
+    use crate::calculate_fuel;
+
+    #[test_case(12, 2 ; "mass 12")]
+    fn test_calculate_fuel(mass: i32, expected: i32) {
+        let fuel = calculate_fuel(mass);
+
+        assert_eq!(fuel, expected)
+    }
+}
