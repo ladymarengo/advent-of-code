3c18b444edf2fb0199152b8d23dbc4ea5b3797b8
Natalia Samoilova <samoilova.natalia.n@gmail.com>
2023-12-04 09:46:19 +0200

    Day 4 part 2

--- 2023-rust/src/04.rs
+++ 2023-rust/src/04.rs
@@ -1,14 +1,15 @@
 use std::fs::read_to_string;
 
+#[derive(Debug)]
 struct Card {
-    winning: Vec<u32>,
-    have: Vec<u32>,
+    copies: usize,
+    matching: usize,
 }
 
 fn main() {
     let input = read_to_string("input/04.txt").unwrap();
 
-    let cards: Vec<Card> = input
+    let mut cards: Vec<Card> = input
         .lines()
         .map(|line| {
             let numbers = line
@@ -23,25 +24,39 @@ fn main() {
                         .collect::<Vec<u32>>()
                 })
                 .collect::<Vec<Vec<u32>>>();
+
+            let matching_numbers = numbers[1]
+                .iter()
+                .filter(|number| numbers[0].contains(*number))
+                .count();
+
             Card {
-                winning: numbers[0].clone(),
-                have: numbers[1].clone(),
+                copies: 1,
+                matching: matching_numbers,
             }
         })
         .collect();
+
     let points: u32 = cards
         .iter()
         .map(|card| {
-            let winning_numbers = card
-                .have
-                .iter()
-                .filter(|number| card.winning.contains(*number))
-                .count();
-            if winning_numbers == 0 {
+            if card.matching == 0 {
                 return 0;
             }
-            u32::pow(2, winning_numbers as u32 - 1)
+            u32::pow(2, card.matching as u32 - 1)
         })
         .sum();
-    println!("{points}");
+
+    println!("First answer is {points}");
+
+    (0..cards.len()).for_each(|i| {
+        (1..=cards[i].matching).for_each(|copy| {
+            if i + copy < cards.len() {
+                cards[i + copy].copies += cards[i].copies
+            }
+        })
+    });
+    let all_cards: usize = cards.iter().map(|card| card.copies).sum();
+
+    println!("Second answer is {all_cards}");
 }
