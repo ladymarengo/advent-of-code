28768fb1933fac2881139fe417e91be8994532dc
Natalia Samoilova <samoilova.natalia.n@gmail.com>
2023-12-05 15:59:08 +0200

    Day 5 part 2

--- 2023-rust/src/03.rs
+++ 2023-rust/src/03.rs
@@ -1,4 +1,3 @@
-use core::num;
 use std::{
     collections::{HashMap, HashSet},
     fs::read_to_string,
--- 2023-rust/src/05.rs
+++ 2023-rust/src/05.rs
@@ -1,4 +1,7 @@
-use std::fs::read_to_string;
+use std::{
+    cmp::{max, min},
+    fs::read_to_string,
+};
 
 #[derive(Debug)]
 struct Range {
@@ -23,7 +26,8 @@ fn main() {
     let maps = almanac[1..]
         .iter()
         .map(|map| {
-            map.lines()
+            let mut ranges = map
+                .lines()
                 .skip(1)
                 .map(|line| {
                     let numbers: Vec<i64> = line
@@ -36,7 +40,9 @@ fn main() {
                         offset: numbers[0] - numbers[1],
                     }
                 })
-                .collect::<Vec<Range>>()
+                .collect::<Vec<Range>>();
+            ranges.sort_by_key(|r| r.source_start);
+            ranges
         })
         .collect::<Vec<Vec<Range>>>();
 
@@ -47,6 +53,47 @@ fn main() {
     locations.sort();
 
     println!("First answer is {}", locations[0]);
+
+    let mut available_ranges = (0..seeds.len())
+        .step_by(2)
+        .map(|i| (seeds[i], seeds[i] + seeds[i + 1] - 1))
+        .collect::<Vec<(i64, i64)>>();
+    available_ranges.sort_by_key(|r| r.0);
+
+    maps.iter()
+        .for_each(|map| find_next_ranges(&mut available_ranges, map));
+
+    println!("Second_answer is {}", available_ranges[0].0);
+}
+
+fn find_next_ranges(available_ranges: &mut Vec<(i64, i64)>, map: &[Range]) {
+    let mut new_ranges: Vec<(i64, i64)> = Vec::new();
+
+    available_ranges.iter().for_each(|range| {
+        let mut start = range.0;
+        let end = range.1;
+        map.iter().for_each(|next_range| {
+            if start <= end {
+                if start < next_range.source_start {
+                    new_ranges.push((start, min(next_range.source_start - 1, end)));
+                    start = min(next_range.source_start - 1, end) + 1;
+                }
+                if end >= next_range.source_start && start <= next_range.source_end {
+                    new_ranges.push((
+                        max(start, next_range.source_start) + next_range.offset,
+                        min(end, next_range.source_end) + next_range.offset,
+                    ));
+                    start = min(end, next_range.source_end) + 1;
+                }
+            }
+        });
+        if start <= end {
+            new_ranges.push((start, end));
+        }
+    });
+
+    new_ranges.sort_by_key(|r| r.0);
+    *available_ranges = new_ranges;
 }
 
 fn find_location(maps: &[Vec<Range>], seed: i64) -> i64 {
