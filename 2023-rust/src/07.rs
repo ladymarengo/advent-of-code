4fafd8b94a782c6891da10a6cfcca6d223e36fda
Natalia Samoilova <samoilova.natalia.n@gmail.com>
2023-12-07 21:12:28 +0200

    Day 7 part 2

--- 2023-rust/src/07.rs
+++ 2023-rust/src/07.rs
@@ -19,7 +19,7 @@ fn main() {
         })
         .collect::<Vec<Hand>>();
 
-    hands.sort_by(compare_cards);
+    hands.sort_by(|a, b| compare_cards(a, b, 1));
     let mut total_winnings: usize = 0;
     hands
         .iter()
@@ -27,14 +27,30 @@ fn main() {
         .for_each(|(i, hand)| total_winnings += (i + 1) * hand.bid);
 
     println!("First answer is {total_winnings}");
+
+    hands.sort_by(|a, b| compare_cards(a, b, 2));
+
+    total_winnings = 0;
+    hands
+        .iter()
+        .enumerate()
+        .for_each(|(i, hand)| total_winnings += (i + 1) * hand.bid);
+
+    println!("Second answer is {total_winnings}");
 }
 
-fn compare_cards(first: &Hand, second: &Hand) -> Ordering {
-    let cards_order = [
-        'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2',
-    ];
-    let first_type = find_type(first);
-    let second_type = find_type(second);
+fn compare_cards(first: &Hand, second: &Hand, part: usize) -> Ordering {
+    let cards_order: [char; 13] = if part == 1 {
+        [
+            'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2',
+        ]
+    } else {
+        [
+            'A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J',
+        ]
+    };
+    let first_type = find_type(first, part);
+    let second_type = find_type(second, part);
 
     match first_type.cmp(&second_type) {
         Ordering::Less => return Ordering::Less,
@@ -62,24 +78,59 @@ fn compare_cards(first: &Hand, second: &Hand) -> Ordering {
     Ordering::Equal
 }
 
-fn find_type(hand: &Hand) -> usize {
+fn find_type(hand: &Hand, part: usize) -> usize {
     let mut elements: HashMap<char, usize> = HashMap::new();
     for e in &hand.cards {
         *elements.entry(*e).or_insert(0) += 1;
     }
-    let amounts = elements.values().cloned().collect::<Vec<usize>>();
-    if amounts.contains(&5) {
-        return 6;
-    } else if amounts.contains(&4) {
-        return 5;
-    } else if amounts.contains(&3) && amounts.contains(&2) {
-        return 4;
-    } else if amounts.contains(&3) {
-        return 3;
-    } else if amounts.iter().filter(|e| **e == 2).count() == 2 {
-        return 2;
-    } else if amounts.contains(&2) {
-        return 1;
+    if part == 1 {
+        let amounts = elements.values().cloned().collect::<Vec<usize>>();
+        if amounts.contains(&5) {
+            return 6;
+        } else if amounts.contains(&4) {
+            return 5;
+        } else if amounts.contains(&3) && amounts.contains(&2) {
+            return 4;
+        } else if amounts.contains(&3) {
+            return 3;
+        } else if amounts.iter().filter(|e| **e == 2).count() == 2 {
+            return 2;
+        } else if amounts.contains(&2) {
+            return 1;
+        }
+    } else {
+        let j = if let Some(x) = elements.get(&'J') {
+            *x
+        } else {
+            0
+        };
+        elements.remove(&'J');
+        let mut amounts = Vec::from_iter(elements.iter());
+        amounts.sort_by_key(|t| t.1);
+        amounts.reverse();
+        let first = if !amounts.is_empty() {
+            *amounts[0].1
+        } else {
+            0
+        };
+        let second = if amounts.len() > 1 { *amounts[1].1 } else { 0 };
+
+        if first == 5 || first + j == 5 || j == 5 {
+            return 6;
+        } else if first == 4 || first + j == 4 || j == 4 {
+            return 5;
+        } else if (first == 3 && (second == 2 || second + j == 2 || j == 2))
+            || (first == 2 && j == 3)
+            || (first + j == 3 && second == 2)
+        {
+            return 4;
+        } else if first == 3 || j == 3 || first + j == 3 {
+            return 3;
+        } else if first == 2 && (second == 2 || second + j == 2 || j == 2) {
+            return 2;
+        } else if first == 2 || j == 2 || first + j == 2 {
+            return 1;
+        }
     }
     0
 }
