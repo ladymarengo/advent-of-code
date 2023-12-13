ff568fb4dc85dee02280eeb55c05cd5caaaa8ca3
Natalia Samoilova <samoilova.natalia.n@gmail.com>
2023-12-13 18:19:45 +0200

    Day 13 part 2

--- 2023-rust/src/13.rs
+++ 2023-rust/src/13.rs
@@ -1,7 +1,7 @@
 use std::fs::read_to_string;
 
 fn main() {
-    let input = read_to_string("input/13_test.txt").unwrap();
+    let input = read_to_string("input/13.txt").unwrap();
 
     let patterns = input.split("\n\n").map(|part| {
         let mut rows = Vec::new();
@@ -19,7 +19,7 @@ fn main() {
     });
 
     let mut sum = 0;
-    patterns.enumerate().for_each(|(p, pattern)| {
+    patterns.clone().enumerate().for_each(|(p, pattern)| {
         println!("Pattern {p}");
         for (i, row) in pattern.0.iter().enumerate() {
             if i < pattern.0.len() - 1 && *row == pattern.0[i + 1] {
@@ -51,4 +51,60 @@ fn main() {
         }
     });
     println!("First answer is {sum}");
+
+    sum = 0;
+    patterns.enumerate().for_each(|(p, pattern)| {
+        println!("Pattern {p}");
+        for (i, _row) in pattern.0.iter().enumerate() {
+            if i < pattern.0.len() - 1 {
+                let mut difference = 0;
+                let mut first = i as i32;
+                let mut second = i + 1;
+
+                while first >= 0 && second < pattern.0.len() {
+                    difference += pattern.0[first as usize]
+                        .iter()
+                        .enumerate()
+                        .filter(|(index, c)| **c != pattern.0[second][*index])
+                        .count();
+                    if difference > 1 {
+                        break;
+                    }
+                    first -= 1;
+                    second += 1;
+                }
+
+                if difference == 1 {
+                    sum += (i + 1) * 100;
+                    break;
+                }
+            }
+        }
+        for (i, _column) in pattern.1.iter().enumerate() {
+            if i < pattern.1.len() - 1 {
+                let mut difference = 0;
+                let mut first = i as i32;
+                let mut second = i + 1;
+
+                while first >= 0 && second < pattern.1.len() {
+                    difference += pattern.1[first as usize]
+                        .iter()
+                        .enumerate()
+                        .filter(|(index, c)| **c != pattern.1[second][*index])
+                        .count();
+                    if difference > 1 {
+                        break;
+                    }
+                    first -= 1;
+                    second += 1;
+                }
+
+                if difference == 1 {
+                    sum += i + 1;
+                    break;
+                }
+            }
+        }
+    });
+    println!("Second answer is {sum}");
 }
