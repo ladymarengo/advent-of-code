c37ded53932279b2354121ed431deed1387577f6
Natalia Samoilova <samoilova.natalia.n@gmail.com>
2023-12-18 20:04:14 +0200

    Day 18 part 2

--- 2023-rust/src/18.rs
+++ 2023-rust/src/18.rs
@@ -1,96 +1,227 @@
 use std::{
     cmp::{max, min},
-    collections::HashSet,
     fs::read_to_string,
 };
+
+#[derive(Debug, PartialEq)]
+struct Rectangle {
+    top: i128,
+    right: i128,
+    down: i128,
+    left: i128,
+}
+
+#[derive(Debug, PartialEq)]
+struct Line {
+    row: (i128, i128),
+    column: (i128, i128),
+    direction: Direction,
+}
+
+#[derive(PartialEq, Copy, Clone, Debug)]
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+}
 
 fn main() {
     let input = read_to_string("input/18.txt").unwrap();
 
-    let mut trench_points: Vec<(i32, i32)> = vec![];
-    let mut trench_path: HashSet<(i32, i32)> = HashSet::new();
+    let mut trench_points: Vec<(i128, i128)> = vec![];
     let mut row = 0;
     let mut column = 0;
+
+    let mut row_points = Vec::new();
+    let mut column_points = Vec::new();
+    let mut lines = Vec::new();
+
     input.lines().for_each(|line| {
         let info: Vec<&str> = line.split(' ').collect();
-        let number = info[1].parse::<i32>().unwrap();
+        let number = i128::from_str_radix(&info[2][2..7], 16).unwrap();
         let current = (row, column);
-        match info[0] {
-            "U" => row -= number,
-            "R" => column += number,
-            "D" => row += number,
-            "L" => column -= number,
+        let direction = match &info[2][7..8] {
+            "3" => Direction::Up,
+            "0" => Direction::Right,
+            "1" => Direction::Down,
+            _ => Direction::Left,
+        };
+        match &info[2][7..8] {
+            "3" => row -= number,
+            "0" => column += number,
+            "1" => row += number,
+            "2" => column -= number,
             _ => (),
         };
         trench_points.push((row, column));
-        for row in min(current.0, row)..=max(current.0, row) {
-            for column in min(current.1, column)..=max(current.1, column) {
-                trench_path.insert((row, column));
+
+        if !row_points.contains(&row) {
+            row_points.push(row);
+        }
+        if !column_points.contains(&column) {
+            column_points.push(column);
+        }
+        lines.push(Line {
+            row: (min(current.0, row), max(current.0, row)),
+            column: (min(current.1, column), max(current.1, column)),
+            direction,
+        });
+    });
+
+    row_points.sort();
+    column_points.sort();
+
+    let mut rectangles: Vec<Rectangle> = Vec::new();
+    lines.iter().for_each(|line| {
+        let step: i128 = match line.direction {
+            Direction::Down | Direction::Left => -1,
+            Direction::Up | Direction::Right => 1,
+        };
+        match line.direction {
+            Direction::Right | Direction::Left => {
+                let mut position = row_points
+                    .iter()
+                    .position(|row| *row == line.row.0)
+                    .unwrap() as i128;
+                loop {
+                    position += step;
+
+                    if position < 0 || position >= row_points.len() as i128 {
+                        position -= step;
+                        break;
+                    } else if lines.iter().any(|other| {
+                        other != line
+                            && other.row.0 == row_points[position as usize]
+                            && !(other.column.0 >= line.column.1 || other.column.1 <= line.column.0)
+                    }) {
+                        break;
+                    }
+                }
+                let rectangle = Rectangle {
+                    top: min(line.row.0, row_points[(position) as usize]),
+                    right: max(line.column.0, line.column.1),
+                    down: max(line.row.0, row_points[(position) as usize]),
+                    left: min(line.column.0, line.column.1),
+                };
+                if !rectangles.contains(&rectangle) {
+                    rectangles.push(rectangle);
+                }
+            }
+            Direction::Up | Direction::Down => {
+                let mut position = column_points
+                    .iter()
+                    .position(|column| *column == line.column.0)
+                    .unwrap() as i128;
+                loop {
+                    position += step;
+                    if position < 0 || position >= column_points.len() as i128 {
+                        position -= step;
+                        break;
+                    } else if lines.iter().any(|other| {
+                        other != line
+                            && other.column.0 == column_points[position as usize]
+                            && !(other.row.0 >= line.row.1 || other.row.1 <= line.row.0)
+                    }) {
+                        break;
+                    }
+                }
+                let rectangle = Rectangle {
+                    top: min(line.row.0, line.row.1),
+                    right: max(line.column.0, column_points[(position) as usize]),
+                    down: max(line.row.0, line.row.1),
+                    left: min(line.column.0, column_points[(position) as usize]),
+                };
+                if !rectangles.contains(&rectangle) {
+                    rectangles.push(rectangle);
+                }
             }
         }
     });
 
-    let row_boundaries = (
-        trench_points.iter().map(|p| p.0).min().unwrap(),
-        trench_points.iter().map(|p| p.0).max().unwrap(),
-    );
-    let column_boundaries = (
-        trench_points.iter().map(|p| p.1).min().unwrap(),
-        trench_points.iter().map(|p| p.1).max().unwrap(),
-    );
+    let amount = (0..row_points.len())
+        .map(|row_pos| {
+            (0..column_points.len())
+                .map(|col_pos| {
+                    let mut new_amount = 0;
+                    new_amount += count_intersection(
+                        &rectangles,
+                        row_points[row_pos],
+                        row_points[row_pos],
+                        column_points[col_pos],
+                        column_points[col_pos],
+                    );
+                    if row_pos < row_points.len() - 1
+                        && col_pos < column_points.len() - 1
+                        && row_points[row_pos + 1] - row_points[row_pos] > 1
+                        && column_points[col_pos + 1] - column_points[col_pos] > 1
+                    {
+                        new_amount += count_intersection(
+                            &rectangles,
+                            row_points[row_pos] + 1,
+                            row_points[row_pos + 1] - 1,
+                            column_points[col_pos] + 1,
+                            column_points[col_pos + 1] - 1,
+                        );
+                        new_amount += count_intersection(
+                            &rectangles,
+                            row_points[row_pos] + 1,
+                            row_points[row_pos + 1] - 1,
+                            column_points[col_pos],
+                            column_points[col_pos],
+                        );
+                        new_amount += count_intersection(
+                            &rectangles,
+                            row_points[row_pos],
+                            row_points[row_pos],
+                            column_points[col_pos] + 1,
+                            column_points[col_pos + 1] - 1,
+                        );
+                    } else if row_pos < row_points.len() - 1
+                        && row_points[row_pos + 1] - row_points[row_pos] > 1
+                    {
+                        new_amount += count_intersection(
+                            &rectangles,
+                            row_points[row_pos] + 1,
+                            row_points[row_pos + 1] - 1,
+                            column_points[col_pos],
+                            column_points[col_pos],
+                        );
+                    } else if col_pos < column_points.len() - 1
+                        && column_points[col_pos + 1] - column_points[col_pos] > 1
+                    {
+                        new_amount += count_intersection(
+                            &rectangles,
+                            row_points[row_pos],
+                            row_points[row_pos],
+                            column_points[col_pos] + 1,
+                            column_points[col_pos + 1] - 1,
+                        );
+                    }
 
-    let map_size = (
-        max(row_boundaries.0, row_boundaries.1) - min(row_boundaries.0, row_boundaries.1) + 1,
-        max(column_boundaries.0, column_boundaries.1)
-            - min(column_boundaries.0, column_boundaries.1)
-            + 1,
-    );
+                    new_amount
+                })
+                .sum::<usize>()
+        })
+        .sum::<usize>();
 
-    // for row in (row_boundaries.0..=row_boundaries.1) {
-    // 	for column in (column_boundaries.0..=column_boundaries.1) {
-    // 		if trench_path.contains(&(row, column)) {
-    // 			print!("#");
-    // 		} else {
-    // 			print!(".");
-    // 		}
-    // 	}
-    // 	println!();
-    // }
-
-    let amount = (map_size.0 + 2) * (map_size.1 + 2)
-        - count_neighbors(&trench_path, row_boundaries, column_boundaries) as i32;
-    println!("First answer is {amount}");
+    println!("{amount}");
 }
 
-fn count_neighbors(
-    path: &HashSet<(i32, i32)>,
-    row_boundaries: (i32, i32),
-    column_boundaries: (i32, i32),
+fn count_intersection(
+    rectangles: &[Rectangle],
+    top: i128,
+    down: i128,
+    left: i128,
+    right: i128,
 ) -> usize {
-    let mut checked: Vec<(i32, i32)> = Vec::new();
-    let mut amount = 0;
-    let mut open = vec![(row_boundaries.0 - 1, column_boundaries.0 - 1)];
-    while let Some(current) = open.pop() {
-        (-1..=1).for_each(|row| {
-            (-1..=1).for_each(|column| {
-                if current.0 + row >= row_boundaries.0 - 1
-                    && current.1 + column >= column_boundaries.0 - 1
-                    && current.0 + row <= row_boundaries.1 + 1
-                    && current.1 + column <= column_boundaries.1 + 1
-                {
-                    let new = (current.0 + row, current.1 + column);
-                    if !path.contains(&new)
-                        && !checked.contains(&new)
-                        && !open.contains(&new)
-                        && new != current
-                    {
-                        open.push(new);
-                    }
-                }
-            })
-        });
-        checked.push(current);
-        amount += 1;
+    if rectangles.iter().any(|rectangle| {
+        top >= rectangle.top
+            && down <= rectangle.down
+            && left >= rectangle.left
+            && right <= rectangle.right
+    }) {
+        return ((down - top + 1) * (right - left + 1)) as usize;
     }
-    amount
+    0
 }
